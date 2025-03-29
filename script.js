ffb2cffb6837affce72968d7ff4e880a5e0ce8ad
diti <ildrit.zenku@icloud.com>
2025-03-29 21:36:40 +0100

    Përmirësuar dizajnin dhe shtuar butonin Kopjo

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
+// Funksioni për të shfaqur ose fshehur linket
 function shfaqLinket() {
-    // Merrni div-in që përmban linket M3U
     var linket = document.getElementById("kanal1-links");
-    
-    // Kontrolloni nëse janë të dukshme ose jo linket
+
     if (linket.style.display === "none") {
         linket.style.display = "block"; // Shfaq linket
     } else {
-        linket.style.display = "none"; // Fshij linket nëse janë të dukshme
+        linket.style.display = "none"; // Fshih linket nëse janë të dukshme
     }
+}
+
+// Funksioni për të kopjuar linkun
+function kopjoLink(button) {
+    var linku = button.previousElementSibling.innerText; // Merr tekstin e linkut
+    var tempInput = document.createElement("input"); // Krijo një input të përkohshëm
+    document.body.appendChild(tempInput);
+    tempInput.value = linku;
+    tempInput.select();
+    document.execCommand("copy"); // Kopjo tekstin në clipboard
+    document.body.removeChild(tempInput);
+
+    // Ndrysho tekstin e butonit për disa sekonda për të treguar që është kopjuar
+    button.innerText = "Kopjuar!";
+    setTimeout(() => {
+        button.innerText = "Kopjo";
+    }, 1500);
 }
